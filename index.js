559d016cc730123665f254b3346ccbb30a6000d3
Alexandr <alexandr.dyomin@yahoo.com>
2024-04-30 10:06:46 +0900

    refactoring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { createServer } from 'http';
 import routes from './routes.js';
+
 const PROTOCOL = process.env.PROTOCOL;
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
@@ -11,6 +12,7 @@ server.listen(PORT, HOST, handleConnection);
 async function handleRequest(req, res) {  
     res.setHeader('Access-Control-Allow-Origin', `http://${HOST}:${PORT}`);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    
     var extention = req.url.match(/\.\w+$/)?.[0];
     if (extention) {
         routes.sendResource(req, res, { extention });
